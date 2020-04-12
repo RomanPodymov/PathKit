12409b5f6af4fdbe4117306e16103558fdc78200
Roman Podymov <podymfrombryansk@yandex.ru>
2020-04-12 17:38:04 +0200

    Added Xcode project

---
+++ Sources/PathKit.h
@@ -0,0 +1,19 @@
+//
+//  PathKit.h
+//  PathKit
+//
+//  Created by Roman Podymov on 12/04/2020.
+//  Copyright © 2020 Kyle Fuller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PathKit.
+FOUNDATION_EXPORT double PathKitVersionNumber;
+
+//! Project version string for PathKit.
+FOUNDATION_EXPORT const unsigned char PathKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathKit/PublicHeader.h>
+
+
